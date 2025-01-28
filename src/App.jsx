7d0676e6594ac245e55358f8cfccd160b9b738e1
Doğukan Tekin <dogukantekin.ce@gmail.com>
2025-01-28 15:43:07 +0300

    Home page In Progress (version 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
-import Order from './components/order'
+import Order from './components/order';
+import Home from './components/home';
 import React from 'react';
 
 function App() {
 
   return (
     <>
+      {/*<Home />*/}
       <Order />
     </>
   )
